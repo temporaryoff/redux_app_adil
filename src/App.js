d1aa45fcd6b9465eec3646e2b76135d3816353a8
Yogeshkumar Patil <ybp345@gmail.com>
2023-08-03 19:51:32 +0530

    Adil Redux Old Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import {increase , decrease} from './actions';
+
 
 function App() {
+  const myState = useSelector ((state) => state.inc_dec);
+  const dispatchabcd = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome To React Redux</h1>
+      <input type="text" value={myState}/>
+      <br /><br />
+      <button onClick={() => dispatchabcd(increase())} className='btn-btn-primary'>Add</button>
+      <button onClick={() => dispatchabcd(decrease())} className='btn-btn-danger'>Minus</button>      
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import inc_dec from './inc_dec';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    inc_dec
+})
+
+export default rootReducer
